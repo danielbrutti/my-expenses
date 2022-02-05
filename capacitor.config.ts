8ecd4c6d56d58b4504f39208a68d504bd3fa17d5
daniel <daniel.brutti@supportronics.com>
2022-02-05 10:44:42 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'myExpenses',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
